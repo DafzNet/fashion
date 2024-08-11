be6ac87c42763a9d808ebb9840017a1a8209610d
DafzNet <50762606+DafzNet@users.noreply.github.com>
2024-08-11 19:25:07 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -70,5 +70,5 @@ def predict():
         if os.path.exists(image_path):
             os.remove(image_path)
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+#if __name__ == "__main__":
+ #   app.run(host="0.0.0.0", port=5000)
